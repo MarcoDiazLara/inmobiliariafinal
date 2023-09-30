614b0352189db0ee5b5c08df588f845c48318aa9
Alejandra2050 <75100495+Alejandra2050@users.noreply.github.com>
2023-09-30 13:09:56 -0600

    ya index completo ya lleva al login y a de registro

---
+++ src/app/web/atajos/atajos.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AtajosComponent } from './atajos.component';
+
+describe('AtajosComponent', () => {
+  let component: AtajosComponent;
+  let fixture: ComponentFixture<AtajosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AtajosComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AtajosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
