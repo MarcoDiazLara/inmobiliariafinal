46ab5cacbdad60d4170871799b782fd96ef414a7
Alejandra2050 <75100495+Alejandra2050@users.noreply.github.com>
2023-10-02 11:49:41 -0600

    Index completo

---
+++ src/app/web/descarga/descarga.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-descarga',
+  templateUrl: './descarga.component.html',
+  styleUrls: ['./descarga.component.css']
+})
+export class DescargaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
