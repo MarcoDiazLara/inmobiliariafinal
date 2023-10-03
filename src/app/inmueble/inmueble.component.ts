b1bf2adf349cef4b3cc1bbf6f7d495f0ad3be02a
MarcoDiazLara <marco.diazl@alumno.buap.mx>
2023-10-03 09:50:47 -0600

    xd

--- src/app/inmueble/inmueble.component.ts
+++ src/app/inmueble/inmueble.component.ts
@@ -14,7 +14,6 @@ import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angula
 
 
 
-  // esto se tomo de ejemplo 
 
 
 
@@ -53,6 +52,7 @@ export class InmuebleComponent implements OnInit {
   asentamiento!: Asentamiento;
   firstFormGroup!: FormGroup;
   secondFormGroup!: FormGroup;
+  tercerFormGroup!: FormGroup;
 
   ngOnInit(){
     this.obtenerDatosInmuebles();
@@ -64,8 +64,27 @@ export class InmuebleComponent implements OnInit {
     this.secondFormGroup = this.formBuilder.group({
       pId_estado: ['',[Validators.required]],
       pId_municipio: ['',[Validators.required]],
-      pId_asentamiento: ['',[Validators.required]]
+      pId_asentamiento: ['',[Validators.required]],
+      p_calle: ['',[Validators.required]]
      })
+    this.tercerFormGroup = this.formBuilder.group({
+      p_nom_inmu:  ['',[Validators.required]],
+      p_desc:  ['',[Validators.required]],
+      p_num_int:  ['',[Validators.required]],
+      p_num_ext:  ['',[Validators.required]],
+      p_terreno:  ['',[Validators.required]],
+      p_constru: ['',[Validators.required]],
+      p_num_recamaras:['',[Validators.required]],
+      p_banos:['',[Validators.required]],
+      p_pisos:['',[Validators.required]], 
+      p_gym: ['',[Validators.required]],
+      p_esta: ['',[Validators.required]],
+      p_jardin: ['',[Validators.required]],
+      p_cocina:['',[Validators.required]],
+      p_alberca: ['',[Validators.required]],
+      p_roof: ['',[Validators.required]],
+    })
+
   }
 
   obtenerDatosInmuebles(){
@@ -96,7 +115,6 @@ export class InmuebleComponent implements OnInit {
    }
 
 updateM(){
-  console.log(this.secondFormGroup.value.pId_estado);
   this.httpService.obtenerMunicipio(this.secondFormGroup.value.pId_estado).subscribe((resp:any)=> {
     if(resp !== 201){
       this.municipio = resp[0].id_Municipio;
@@ -105,12 +123,9 @@ updateM(){
    },(err)=>{
     console.log(err);
    })
-  
- 
 }
 
 updateA(){
-  
   this.httpService.obtenerAsentamiento(this.secondFormGroup.value.pId_estado,
     this.secondFormGroup.value.pId_municipio).subscribe((resp:any)=> {
     if(resp !== 201){
@@ -125,18 +140,67 @@ updateA(){
 }
 
 
+xd(){
+  console.log(this.tercerFormGroup.value.p_gym);
+}
+
 
   onFileSelected(event: any): void {
     const file = event.target.files[0];
     if (file) {
       this.fileName = file.name;
       // Aquí puedes realizar acciones adicionales con el archivo seleccionado, como enviarlo a un servidor.
+      console.log(file.name);
     } else {
       this.fileName = "";
     }
 
 
 
+  }
+
+  subirInmueble(){
+    let p_nom_inmueble = this.tercerFormGroup.value.p_nom_inmu;
+    let p_desc_inmueble = this.tercerFormGroup.value.p_desc;
+    let p_calle1 = this.secondFormGroup.value.p_calle;
+    let p_num_ext1 = this.tercerFormGroup.value.p_num_ext;
+    let p_num_int1 = this.tercerFormGroup.value.p_num_int;
+    let p_terreno1 = this.tercerFormGroup.value.p_terreno;
+    let p_construccion = this.tercerFormGroup.value.p_constru;
+    let p_recamara = this.tercerFormGroup.value.p_num_recamaras;
+    let p_bano = this.tercerFormGroup.value.p_banos;
+    let p_cocina1 = this.tercerFormGroup.value.p_cocina;
+    let p_num_pisos = this.tercerFormGroup.value.p_pisos;
+    let p_antiguedad = "2";
+    let p_acabados = "Nose xd";
+    let p_alberca1 = this.tercerFormGroup.value.p_alberca;
+    let p_jardin1 = this.tercerFormGroup.value.p_jardin;
+    let p_gym1 = this.tercerFormGroup.value.p_gym; 
+    let p_roof = this.tercerFormGroup.value.p_roof;
+    let p_estacionamiento = this.tercerFormGroup.value.p_esta;
+    let p_ubi_maps = "Hola ";
+    let p_pic_1 = "imagen 1";
+    let p_pic_2 = "imagen 2";
+    let p_pic_3 = "imagen 3";
+    let p_pic_4 = "imagen 4";
+    let p_pic_5 = "imagen 5";
+    let p_360 = "imagen 360";
+    let p_video = "video 1";
+    let p_id_asentamiento = this.secondFormGroup.value.pId_asentamiento;
+    let p_id_tipo_inmueble = this.firstFormGroup.value.pId_Tipo_Inmueble;
+    
+
+    
+    this.httpService.registrarInmuebles(p_nom_inmueble,p_desc_inmueble,p_calle1,p_num_ext1,p_num_int1,p_terreno1,
+      p_construccion,p_recamara,p_bano,p_cocina1,p_num_pisos, p_antiguedad, p_acabados,p_alberca1, p_jardin1,p_gym1,
+      p_roof,p_estacionamiento, p_ubi_maps,p_pic_1, p_pic_2, p_pic_3, p_pic_4, p_pic_5, p_360, p_video, p_id_asentamiento,p_id_tipo_inmueble
+      ).subscribe((data: any) =>{
+      if(data == 1){
+        alert("Se subio el inmueble");
+      } else{
+        alert("Error al subir inmueble");
+      }
+       })
   }
 
        
