5c9b07dbe8132fb42fd6c50e273da3e9a1806d60
MarcoDiazLara <marco.diazl@alumno.buap.mx>
2023-10-04 19:20:25 -0600

    Eliminacion de asesores funcional

--- src/app/master-admin/eliminarasesores/eliminarasesores.component.ts
+++ src/app/master-admin/eliminarasesores/eliminarasesores.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Asesores } from 'src/app/services/Interface/Interfaces';
+import { HttpService } from 'src/app/services/http/http.service';
 
 @Component({
   selector: 'app-eliminarasesores',
@@ -8,26 +10,42 @@ import { Component, OnInit } from '@angular/core';
 export class EliminarasesoresComponent implements OnInit {
 
   searchTerm: string = '';
-  asesores: any[] = [
-    { nombre: 'Asesor 1' },
-    { nombre: 'Asesor 2' },
-    { nombre: 'Asesor 3' }
-  ];
+  asesores : Asesores[] = [];
+  asesor !: Asesores;
   confirmacionVisible: boolean = false;
   asesorSeleccionado: any;
+  id: any;
 
-  buscarAsesor() {
-    // Agregar lógica de búsqueda aquí
+  constructor(private httpService: HttpService) { }
+
+
+  ngOnInit(): void {
   }
 
-  mostrarConfirmacion(asesor: any) {
+  buscarAsesor() {
+    let p_nombre = this.searchTerm;
+    this.httpService.obtenerAsesores(p_nombre).subscribe((resp : any)=> {
+      if(resp !== 201){
+        this.asesores = resp;
+      }
+     },(err)=>{
+      console.log(err);
+     
+    })
+  }
+
+  mostrarConfirmacion(nombre:string,id: string) {
     this.confirmacionVisible = true;
-    this.asesorSeleccionado = asesor;
+    this.asesorSeleccionado = nombre;
+    this.id = id;
   }
 
   eliminarAsesor() {
-    // Agregar lógica de eliminación aquí
-    this.confirmacionVisible = false;
+  this.httpService.eliminarAsesores(this.id).subscribe((data : any)=>{
+    if(data = 1){
+      alert("Se elimino el Asesor")
+    }
+  })
   }
 
   cancelarEliminar() {
@@ -35,9 +53,7 @@ export class EliminarasesoresComponent implements OnInit {
   }
 
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  
 
 }
