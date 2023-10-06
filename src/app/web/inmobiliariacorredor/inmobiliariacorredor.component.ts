107183d91f1554635da7761a8dc3360f1fc657d0
Alejandra2050 <75100495+Alejandra2050@users.noreply.github.com>
2023-10-06 13:38:06 -0600

    perfiles Terminado

--- src/app/web/inmobiliariacorredor/inmobiliariacorredor.component.ts
+++ src/app/web/inmobiliariacorredor/inmobiliariacorredor.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inmobiliariacorredor',
   templateUrl: './inmobiliariacorredor.component.html',
-  styleUrls: ['./inmobiliariacorredor.component.scss']
+  styleUrls: ['./inmobiliariacorredor.component.css']
 })
 export class InmobiliariacorredorComponent implements OnInit {
 
