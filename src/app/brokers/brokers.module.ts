9d05627d224d5910b629bd766dc5b30805139712
AlmaJimenezGonz <144948872+AlmaJimenezGonz@users.noreply.github.com>
2023-10-09 15:03:57 -0600

    Actualizacion

--- src/app/brokers/brokers.module.ts
+++ src/app/brokers/brokers.module.ts
@@ -4,6 +4,26 @@ import { BrokersRoutingModule } from './brokers-routing.module';
 import { BrokersComponent } from './brokers.component';
 import { PerfilbrokersComponent } from './perfilbrokers/perfilbrokers.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+
+/*tablas */
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { ErrorStateMatcher, MatNativeDateModule } from '@angular/material/core';
+/end/
+
 @NgModule({
   declarations: [
     BrokersComponent,
@@ -11,7 +31,24 @@ import { PerfilbrokersComponent } from './perfilbrokers/perfilbrokers.component'
   ],
   imports: [
     CommonModule,
-    BrokersRoutingModule
+    BrokersRoutingModule,
+    CommonModule,
+    MatToolbarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatStepperModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
   ]
 })
 export class BrokersModule { }
