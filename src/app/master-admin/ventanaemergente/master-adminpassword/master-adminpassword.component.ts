5c13e6f6694d92a6d03ac18e1e6e00479e0b6ad1
AlmaJimenezGonz <144948872+AlmaJimenezGonz@users.noreply.github.com>
2023-10-21 09:52:24 -0600

    actualizacion

--- src/app/master-admin/ventanaemergente/master-adminpassword/master-adminpassword.component.ts
+++ src/app/master-admin/ventanaemergente/master-adminpassword/master-adminpassword.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { HttpService } from 'src/app/services/http/http.service';
 
 @Component({
   selector: 'app-master-adminpassword',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterAdminpasswordComponent implements OnInit {
 
-  constructor() { }
+  formGeneral!:FormGroup;
+  loading = false;
+  hide = true;
+  hide2 = true;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private dialog: MatDialog,
+    private httpService: HttpService,
+  ) { }
 
   ngOnInit(): void {
   }
+
+  closeDialog() {
+    this.dialog.closeAll();
+  }
+
+  actualizarpassword(){
+    if (this.formGeneral){
+     let contrasena = this.formGeneral.value.password;
+     let contrasena2 = this.formGeneral.value.password2;
+     let Id_Usuario = localStorage.getItem("Id_Usuario");
+     if(contrasena == contrasena2){
+      this.httpService.cambiarContra(Id_Usuario,contrasena).subscribe(()=>{ alert("Se ha cambiado la contraseña");});
+      
+      this.closeDialog();
+    }
+    else{
+      alert("Las contraseñas no son iguales")
+    }
+   }
+  } 
 
 }
