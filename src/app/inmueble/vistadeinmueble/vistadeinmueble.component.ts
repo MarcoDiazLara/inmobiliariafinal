52d1528532d18683759bb9c09d35a28d9df52b33
IIITunersIII <sanchezmota09@gmail.com>
2023-10-27 11:46:04 -0600

    filtros

--- src/app/inmueble/vistadeinmueble/vistadeinmueble.component.ts
+++ src/app/inmueble/vistadeinmueble/vistadeinmueble.component.ts
@@ -13,21 +13,12 @@ import { inmueblesBuscados } from 'src/app/services/Interface/Interfaces';
   styleUrls: ['./vistadeinmueble.component.css'],
 
 })
-export class VistadeinmuebleComponent implements OnInit {
-  public showPrecio: boolean = false;
-  public showToferta: boolean = false;
-  public showInmueble: boolean = false;
-  public showRecamaras: boolean = false;
-  public showSearch: boolean = false;
-  public showPrecioEjemplo: boolean = false;
-  public showFiltros: boolean = false;
 
-  panelOpenState = false;
+export class VistadeinmuebleComponent implements OnInit {
+
   
   control = new FormControl('');
-  streets: string[] = [
-    'Guarderia', 'Escuela', 'Gimnasio', 'Centro comercial'];
-  filteredStreets: Observable<string[]>;
+  
 
   firstFormGroup!: FormGroup;
   precios!: FormGroup
@@ -77,97 +68,14 @@ export class VistadeinmuebleComponent implements OnInit {
 
 
   constructor(private el: ElementRef, private router: Router, private http: HttpService, private renderer: Renderer2, private route: ActivatedRoute, private formBuilder: FormBuilder) {
-    this.filteredStreets = new Observable<string[]>();
+    
   }
 
 
   @HostListener('document:click', ['$event'])
 
 
-  onDocumentClick(event: MouseEvent) {
-    // Verifica si el clic no fue dentro del botón de Precio ni dentro de la lista de Precio
-    if (!this.el.nativeElement.querySelector('.navbar').contains(event.target) &&
-      !this.el.nativeElement.querySelector('.dropdown-precio, .dropdown-Toferta, .dropdown-inmueble, .dropdown-Recamaras, .dropdown-search, .dropdown-Filtros').contains(event.target)) {
-      // Si se hizo clic fuera del botón y fuera de la lista de Precio, oculta la lista
-      this.showPrecio = false;
-      this.showToferta = false;
-      this.showInmueble = false;
-      this.showRecamaras = false;
-      this.showSearch = false;
-      this.showFiltros = false;
-
-
-    }
-  }
-
-  // Función para mostrar/ocultar la lista de Precio
-  togglePrecio() {
-    this.showPrecio = !this.showPrecio;
-    // Oculta las otras listas
-    this.showToferta = false;
-    this.showInmueble = false;
-    this.showRecamaras = false;
-    this.showSearch = false;
-    this.showFiltros = false;
-  }
-
-  // Función para mostrar/ocultar la lista de Tipo de Oferta
-  toggleToferta() {
-    this.showToferta = !this.showToferta;
-    // Oculta las otras listas
-    this.showPrecio = false;
-    this.showInmueble = false;
-    this.showRecamaras = false;
-    this.showSearch = false;
-    this.showFiltros = false;
-  }
-
-  // Función para mostrar/ocultar la lista de Inmueble
-  toggleInmueble() {
-    this.showInmueble = !this.showInmueble;
-    // Oculta las otras listas
-    this.showPrecio = false;
-    this.showToferta = false;
-    this.showRecamaras = false;
-    this.showSearch = false;
-    this.showFiltros = false;
-  }
-
-  // Función para mostrar/ocultar la lista de Recamaras
-  toggleRecamaras() {
-    this.showRecamaras = !this.showRecamaras;
-    // Oculta las otras listas
-    this.showPrecio = false;
-    this.showToferta = false;
-    this.showInmueble = false;
-    this.showSearch = false;
-    this.showFiltros = false;
-  }
-
-  // Función para mostrar/ocultar la barra de resultados de escritura en busqueda
-  toggleSearch() {
-    this.showSearch = !this.showSearch;
-    // Oculta las otras listas
-    this.showPrecio = false;
-    this.showToferta = false;
-    this.showInmueble = false;
-    this.showRecamaras = false;
-    this.showFiltros = false;
-
-
-  }
-  // Función para mostrar/ocultar la barra de resultados de escritura en busqueda
-  toggleFiltros() {
-    this.showFiltros = !this.showFiltros;
-    // Oculta las otras listas
-    this.showPrecio = false;
-    this.showToferta = false;
-    this.showInmueble = false;
-    this.showRecamaras = false;
-    this.showSearch = false;
-  }
-
-
+ 
 
 
 
@@ -246,19 +154,6 @@ export class VistadeinmuebleComponent implements OnInit {
 
   ResultadoosBusquedaC() {
     this.router.navigate(["/inmueble/vista"], { queryParams: { 'action': 'compra', 'tpropiedad': this.tpropiedad, 'ubicacion': this.ubicacion} });
-  }
-
-
-  // Función para el autocompletado de las caracteristicas en el boton de filtros avanzados
-  private _filter(value: string): string[] {
-    const filterValue = this._normalizeValue(value);
-    return this.streets.filter(street => this._normalizeValue(street).includes(filterValue));
-  }
-
-
-  // Función para el autocompletado de las caracteristicas en el boton de filtros avanzados
-  private _normalizeValue(value: string): string {
-    return value.toLowerCase().replace(/\s/g, '');
   }
 
   mostrarMunicipios() {
