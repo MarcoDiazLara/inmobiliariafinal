3b28938911792d6654cbb09b61d6b9f234496b80
IsaacFloresRojas <aifr1isaac@gmail.com>
2023-11-10 17:45:06 -0600

    blog "completo" xd

--- src/app/web/desarrollob/desarrollob.component.ts
+++ src/app/web/desarrollob/desarrollob.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-desarrollob',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesarrollobComponent implements OnInit {
 
-  constructor() { }
+  constructor(  private router:Router ) {  }
 
   ngOnInit(): void {
   }
 
+  back(){
+    this.router.navigate(["/web/blog"]);
+  }
 }
