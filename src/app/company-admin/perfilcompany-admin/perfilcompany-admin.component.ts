eacf1c69289cd3cc170f88610c318f52e99cda5b
Alejandra2050 <75100495+Alejandra2050@users.noreply.github.com>
2023-12-06 14:52:02 -0600

    xd roles

--- src/app/company-admin/perfilcompany-admin/perfilcompany-admin.component.ts
+++ src/app/company-admin/perfilcompany-admin/perfilcompany-admin.component.ts
@@ -13,104 +13,27 @@ import { CompanyAdminpasswordComponent } from '../ventanaemergente/company-admin
   templateUrl: './perfilcompany-admin.component.html',
   styleUrls: ['./perfilcompany-admin.component.scss']
 })
-export class PerfilcompanyAdminComponent implements OnInit {
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  toppings = new FormControl('');
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+export class PerfilcompanyAdminComponent  {
+  saludo: string = '';
 
-  formGeneral!: FormGroup;
-  loading = false;
-  hide2 = true;
+  ngOnInit() {
+    const fecha = new Date();
+    const hora = fecha.getHours();
 
-  datos!: infoUsuario;
-  datos2!: infoUsuario;
-
-  constructor(
-    public dialog: MatDialog,
-    private formBuilder: FormBuilder,
-    // private dialog: MatDialog,
-    private httpService: HttpService,
-  ) { }
-
-  ngOnInit(): void {
-    this.obtenerInfo();
-    this.formGeneral = this.formBuilder.group({
-      nombre:['',[Validators.required]],
-      apellidopaterno:['',[Validators.required]],
-      apellidomaterno:['',[Validators.required]],
-      curp:['',[Validators.required]],
-      rfc:['',[Validators.required]],
-      contactoprincipal:['',[Validators.required]],
-      contactoemergencia:['',[Validators.required]],
-      email:['',[Validators.required]],
-      nombreusuario:['',[Validators.required]],
-      imageInput:['',[Validators.required]],
-    });
-  }
-
-  // Apartado Dialog Pantalla Emergente 
-
-  openpassword() {
-
-    const dialogRef = this.dialog.open(CompanyAdminpasswordComponent, {
-      width: '60vh',
-      height: 'auto',
-      disableClose: true
-    });
-  }
-
-  openDialog(): void {
-
-  }
-
-  Guardardatos() {
-
-
-    if (this.formGeneral) {
-      let id = localStorage.getItem("Id_Usuario");
-      let nombre = this.formGeneral.value.nombre;
-      let apellidopaterno = this.formGeneral.value.apellidopaterno;
-      let apellidomaterno = this.formGeneral.value.apellidomaterno;
-      let curp = this.formGeneral.value.curp;
-      let rfc = this.formGeneral.value.rfc;
-      let contactoprincipal = this.formGeneral.value.contactoprincipal;
-      let contactoemergencia = this.formGeneral.value.contactoemergencia;
-      let email = this.formGeneral.value.email;
-      let nombreusuario = this.formGeneral.value.nombreusuario;
-      let imageInput = this.formGeneral.value.imageInput;
-  //id , nombre , apellidopaterno , apellidomaterno , curp , rfc ,  contactoprincipal , contactoemergencia ,  email ,  nombreusuario , imageInput
-      this.httpService.updateInfoUsuario(id , nombre , apellidopaterno , apellidomaterno , curp , rfc ,  contactoprincipal , contactoemergencia ,  email ,  nombreusuario ).subscribe((data : any)=>{
-        if(data ==1){
-          alert("Se actualizo usuario");
-        }else{
-          alert("Error al actualizar");
-        }
-      })
-
+    if (hora >= 0 && hora < 12) {
+      this.saludo = '¡Buenos días!';
+    } else if (hora >= 12 && hora < 18) {
+      this.saludo = '¡Buenas tardes!';
+    } else {
+      this.saludo = '¡Buenas noches!';
     }
   }
+ 
 
-  obtenerInfo(){
-    this.httpService.obtenerInfoUsuario(localStorage.getItem("Id_Usuario")).subscribe((data : any) =>
-    {if(data ==201){
-      alert("Error al leer usuario");
-    }else{
-    
-      this.datos = data;
-      this.obtenerInfo2();
-    }})
-  }
-  obtenerInfo2(){
-    this.httpService.obtenerInfoUsuario2(localStorage.getItem("Id_Usuario")).subscribe((data : any) =>
-    {if(data ==201){
-      alert("Error al leer usuario");
-    }else{
-      
-      this.datos.Nombre_Usuario = data.Nombre_Usuario;
-      this.datos.Img_Profile = data.Img_Profile;
-    }})
-  }
+  constructor(
 
+  ) { }
+
+ 
 
 }
