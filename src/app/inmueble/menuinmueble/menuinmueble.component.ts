94d842935da1f4bbe23be951a14ff392f818c2f6
santosvazquezubaldo <108352083+SantosVazquez@users.noreply.github.com>
2023-12-19 11:18:28 -0600

    menu dueño

--- src/app/inmueble/menuinmueble/menuinmueble.component.ts
+++ src/app/inmueble/menuinmueble/menuinmueble.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpService } from 'src/app/services/http/http.service';
-import { MatDialog,} from '@angular/material/dialog';
+import { MatDialog, } from '@angular/material/dialog';
 import { PlanesComponent } from '../planes/planes.component';
 
 @Component({
@@ -9,72 +9,73 @@ import { PlanesComponent } from '../planes/planes.component';
   templateUrl: './menuinmueble.component.html',
   styleUrls: ['./menuinmueble.component.scss']
 })
-export class MenuinmuebleComponent  {
-   
+export class MenuinmuebleComponent {
+
   isLoggedIn: boolean = false;
 
-  constructor(private router: Router,private httpService: HttpService,public dialog: MatDialog) { }
-
- 
+  constructor(private router: Router, private httpService: HttpService, public dialog: MatDialog) { }
 
 
-  Panel(){
+
+
+  Panel() {
     let entrada = localStorage.getItem("Id_Tipo_Usuario");
-    if(entrada == "6" || entrada == "1"){
+    if (entrada == "6" || entrada == "1") {
       this.router.navigate(["/superUsuario/perfil"]);
-    }else if (entrada == "2"){
+    } else if (entrada == "2") {
       this.router.navigate(["/usuario/perfil"]);
-    }else if (entrada == "3"){
-      this.router.navigate(["/asesor"]);
-    }else if( entrada == "4"){
+    } else if (entrada == "3") {
+      this.router.navigate(["/asesor/Perfil"]);
+    } else if (entrada == "4") {
       this.router.navigate(["cliente/cliente/Perfil"]);
-    }else if ( entrada == "5"){
+    } else if (entrada == "10") {
       this.router.navigate(["/Company/Perfil"]);
     }
-
+    else if (entrada == "5") {
+      this.router.navigate(["/Dueno/bienvenida"]);
+    }
   }
-  Editar(){
-    this.router.navigate(["cliente/cliente/Perfil"]),{ replaceUrl: true };
+  Editar() {
+    this.router.navigate(["cliente/cliente/Perfil"]), { replaceUrl: true };
   }
-  cerrar(){
+  cerrar() {
     //this.httpService.setGlobalVariable(false);
-    const itemsToRemove =[
+    const itemsToRemove = [
       "Nombre_Usuario",
       "Id_Usuario",
       "Id_Tipo_Usuario"
     ];
-    itemsToRemove.forEach( item => {
+    itemsToRemove.forEach(item => {
       localStorage.removeItem(item);
     })
     this.httpService.setGlobalVariable(false);
     this.router.navigate(["/web"]);
-   }
-   Espacio(){
+  }
+  Espacio() {
     this.router.navigate(['inmueble/menuinmueble']);
   }
 
-  Avisos(){
+  Avisos() {
     this.router.navigate(['inmueble/avisos']);
-    
+
   }
 
 
-  Contratos()
-{
-  this.router.navigate(['inmueble/contratos']);
+  Contratos() {
+    this.router.navigate(['inmueble/contratos']);
 
 
-}
+  }
 
-home(){
-  this.router.navigate(['/web']);
- }
- planes(): void {
-  const dialogRef = this.dialog.open(PlanesComponent, {
+  home() {
+    this.router.navigate(['/web']);
+  }
+  planes(): void {
+    const dialogRef = this.dialog.open(PlanesComponent, {
 
-  });
+    });
 
- }
+  }
 }
 
 
