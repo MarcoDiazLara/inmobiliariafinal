8e8709cbfb73add45dddf6dcef9bd901d88d8d87
santosvazquezubaldo <108352083+SantosVazquez@users.noreply.github.com>
2023-12-21 13:09:24 -0600

    change arvicesSpace

--- src/app/arvice-space/arvice-space.component.ts
+++ src/app/arvice-space/arvice-space.component.ts
@@ -11,16 +11,16 @@ export class ArviceSpaceComponent implements OnInit {
   mobileQuery: MediaQueryList;
   nombre: any;
   fillerNav = [
-    { name: "Perfil", route: "perfil", icon: "home", label: "Hola", number: '0' },
-    { name: "Notificaciones", route: "notificacionbroker", icon: "notifications", label: "Notificaciones", number: '0' },
+    { name: "Home", route: "Arvice", icon: "home", label: "Home", number: '0' },
+    { name: "Register", route: "notificacionbroker", icon: "groups", label: "Registrar Usuario", number: '0' },
     //{ name: "Asignar-reasignar", route: "asignar-reasignar", icon: "groups", label: "Asignar Reasignar", number: '0' },
-    { name: "Calendario", route: "calendario", icon: "date_range", label: " Agenda", number: '0' },
+    //{ name: "Calendario", route: "calendario", icon: "date_range", label: " Agenda", number: '0' },
     // { name: "Hitobroker", route: "Hitobroker",icon: "event" , label: "Fecha Hito",number:'0' },
-    { name: "Inventario", route: "invesntario", icon: "inventory", label: "Inventario", number: '0' },
-    { name: "Asignar Users", route: "asignarUsers", icon: "event", label: "Asginar Reasignar Usuarios", number: '0' },
-    { name: "fechashito", route: "fechashito", icon: "collections_bookmark", label: "fechashito ", number: '0' },
-    { name: "Catalogoinmueble", route: "catalogoinmueble", icon: "collections_bookmark", label: "Sube tu Catalogo de Inmueble", number: '0' },
-    { name: "Salir", route: "web", icon: "logout", label: "Salir", number: '1' }
+    //{ name: "Inventario", route: "invesntario", icon: "inventory", label: "Inventario", number: '0' },
+    //  { name: "Asignar Users", route: "asignarUsers", icon: "event", label: "Asginar Reasignar Usuarios", number: '0' },
+    // { name: "fechashito", route: "fechashito", icon: "collections_bookmark", label: "fechashito ", number: '0' },
+    //{ name: "Catalogoinmueble", route: "catalogoinmueble", icon: "collections_bookmark", label: "Sube tu Catalogo de Inmueble", number: '0' },
+    //{ name: "Salir", route: "web", icon: "logout", label: "Salir", number: '1' }
 
   ];
 
