aa1ff1e69e581b435d1df9b6657572e497e768df
Alejandra2050 <75100495+Alejandra2050@users.noreply.github.com>
2023-12-23 12:22:50 -0600

    Modificacion del 3D

--- src/app/inmueble/requisitos3d/requisitos3d.component.ts
+++ src/app/inmueble/requisitos3d/requisitos3d.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular
 import { FormControl } from '@angular/forms';
 import {  Input } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {  FormArray } from '@angular/forms';
+
 
 
 
@@ -16,10 +18,14 @@ interface ColorOption {
   styleUrls: ['./requisitos3d.component.scss']
 })
 export class Requisitos3dComponent  implements OnInit {
-  formGeneral!:FormGroup;
-  
+  form: FormGroup;
+  // usuarios: FormArray;
+  forms: FormGroup[] = [];
+   formGeneral!:FormGroup;
   numberOfForms: number = 1; // Valor inicial
   dynamicForms: FormGroup[] = [];
+  miFormulario!: FormGroup;
+  
 
   selectedColor: string = '';
   colorOptions: ColorOption[] = [
@@ -30,10 +36,52 @@ export class Requisitos3dComponent  implements OnInit {
   ];
   
   constructor(private fb: FormBuilder , public dialog: MatDialog) {
-    this.initializeForms(0); // Specify the desired number of forms
+    this.form = this.fb.group({
+      usuarios: this.fb.array([])
+    });
+    // this.generateForms();
+    // this.initializeForms(0); // Specify the desired number of forms
+
+
+  }
+ 
+
+  
+  get items(): FormArray {
+    return this.miFormulario.get('items') as FormArray;
   }
 
+  agregarItem() {
+    this.items.push(this.fb.group({
+      // Definir los campos del formulario aquí
+    }));
+  }
+
+  eliminarItem(index: number) {
+    this.items.removeAt(index);
+  }
+
+
+
+
+
+
+
+  
+
   ngOnInit() {
+    this.miFormulario = this.fb.group({
+      items: this.fb.array([]),
+      name: ['', [Validators.required]],
+      Nombre_de_habitacion: ['', [Validators.required]],
+      Elementos_del_Inmueble:['',[Validators.required]],
+      Acabados: ['', [Validators.required]],
+      Color:['', [Validators.required]],
+      tipo_material:['', [Validators.required]],
+      Largo:['', [Validators.required]],
+      Ancho:['', [Validators.required]],
+    });
+ 
 //     this.formGeneral = this.fb.group({
 //       name: ['', [Validators.required]],
 //       Nombre_de_habitacion: ['', [Validators.required]],
@@ -45,30 +93,53 @@ export class Requisitos3dComponent  implements OnInit {
 //       Ancho:['', [Validators.required]],
 //  });
   }
-  generateForms() {
-    this.dynamicForms = [];
-    this.initializeForms(this.numberOfForms);
-  }
-  private initializeForms(numberOfForms: number) {
-    for (let i = 0; i < numberOfForms; i++) {
-      const form = this.fb.group({
-      name: ['', [Validators.required]],
-      Nombre_de_habitacion: ['', [Validators.required]],
-      Elementos_del_Inmueble:['',[Validators.required]],
-      Acabados: ['', [Validators.required]],
-      Color:['', [Validators.required]],
-      tipo_material:['', [Validators.required]],
-      Largo:['', [Validators.required]],
-      Ancho:['', [Validators.required]],
-      });
+  // generateForms() {
+  //   this.dynamicForms = [];
+  //   this.initializeForms(this.numberOfForms);
+  // }
 
-      this.dynamicForms.push(form);
-    }
+
+ 
+
+  get usuarios() {
+    return this.form.get('usuarios') as FormArray;
   }
-  clearForms() {
-    this.dynamicForms = [];
-    this.numberOfForms = 1; // Puedes ajustar esto según tus necesidades
+
+  agregarUsuario() {
+    this.usuarios.push(this.fb.group({
+      username: ['']
+    }));
   }
+
+  eliminarUsuario(index: number) {
+    this.usuarios.removeAt(index);
+  }
+
+
+
+  // private initializeForms(numberOfForms: number) {
+  //   for (let i = 0; i < numberOfForms; i++) {
+  //     const form = this.fb.group({
+  //        Numero_de_Cuartos: ['', [Validators.required]],
+  //     Numero_de_Elementos: ['', [Validators.required]],
+  //     name: ['', [Validators.required]],
+  //     Nombre_de_habitacion: ['', [Validators.required]],
+  //     Elementos_del_Inmueble:['',[Validators.required]],
+  //     Acabados: ['', [Validators.required]],
+  //     Color:['', [Validators.required]],
+  //     tipo_material:['', [Validators.required]],
+  //     Largo:['', [Validators.required]],
+  //     Ancho:['', [Validators.required]],
+  //     });
+
+  //    this.dynamicForms.push(form);
+  //    }
+  // }
+
+  // clearForms() {
+  //   this.dynamicForms = [];
+  //   this.numberOfForms = 1; // Puedes ajustar esto según tus necesidades
+  // }
 
   CerraDialogo(){
     this.dialog.closeAll();
