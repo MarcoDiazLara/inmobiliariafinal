a19596e9336413a25eaddaba8e556670ca7e4255
santosvazquezubaldo <108352083+SantosVazquez@users.noreply.github.com>
2024-01-04 09:43:12 -0600

    update arviceSpace

--- src/app/arvice-space/ventanas-emergentes/ventanas-emergentes.component.ts
+++ src/app/arvice-space/ventanas-emergentes/ventanas-emergentes.component.ts
@@ -13,7 +13,7 @@ export class VentanasEmergentesComponent {
   selectedArchiveAndroid!: FileList;
   //seleccionar imagen iphone
   selectedArchive2IOS!: FileList;
-
+  datosData: [] = [];
   constructor(private httpService: HttpService, private dialog: MatDialog, private httpClient: HttpClient) { }
 
   closeDialog(): void {
@@ -23,6 +23,10 @@ export class VentanasEmergentesComponent {
   //-------------------Asignar archivos seleccionados a variables globales-----------------------------------------------------
   onFileChange(event: any): void {
     this.selectedArchiveAndroid = event.target.files;
+
+  }
+  selectFile(event: any): void {
+
     this.selectedArchive2IOS = event.target.files;
   }
   //------------------------------------------------------------------------------------
@@ -34,8 +38,8 @@ export class VentanasEmergentesComponent {
       formDataAndroid.append('images[]', this.selectedArchiveAndroid[i]);
     }
 
-    for (let i = 0; i < this.selectedArchive2IOS.length; i++) {
-      formDataIOS.append('images[]', this.selectedArchive2IOS[i]);
+    for (let j = 0; j < this.selectedArchive2IOS.length; j++) {
+      formDataIOS.append('images[]', this.selectedArchive2IOS[j]);
     }
 
     this.httpClient.post('https://inmobiliaria.arvispace.com/servicios/ser_subirArchvosAndroid.php', formDataAndroid)
@@ -70,7 +74,7 @@ export class VentanasEmergentesComponent {
 
   subirArchivo() {
     let date = new Date();
-    this.subirArchivo();
+    this.subir_imagenes();
 
     let dia = date.getDate();
     let dia1 = date.getDate().toString();;
@@ -84,18 +88,19 @@ export class VentanasEmergentesComponent {
     }
     let anio = date.getFullYear().toString();
     let nom_aux = anio + mes1 + dia1;
-    let rutaUno = "https://inmobiliaria.arvispace.com/imagenes/" + nom_aux + this.selectedArchiveAndroid[0].name;
-    let rutaDos = "https://inmobiliaria.arvispace.com/imagenes/" + nom_aux + this.selectedArchive2IOS[0].name;
+    let rutaUno = "https://inmobiliaria.arvispace.com/resources/assetbundle/ios/" + nom_aux + this.selectedArchiveAndroid[0].name;
+    let rutaDos = "https://inmobiliaria.arvispace.com/resources/assetbundle/android/" + nom_aux + this.selectedArchive2IOS[0].name;
     let idInmueble = localStorage.getItem("idInmuebleArviceSpace");
     this.httpService.insertarArchivoArvice(idInmueble, rutaUno, rutaDos).subscribe((data: any) => {
-
-      if (data == 1) {
-
-
-      } else {
+      this.datosData = data;
+      alert(this.datosData.length);
+      // if (data == 1) {
 
 
-      }
+      // } else {
+
+
+      // }
     })
   }
 
