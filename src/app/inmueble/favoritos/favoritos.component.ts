65925f785fd3776533f21bfb23f818648523d174
betosau <betosau2112@gmail.com>
2024-01-10 17:19:36 -0600

    Favoritos

--- src/app/inmueble/favoritos/favoritos.component.ts
+++ src/app/inmueble/favoritos/favoritos.component.ts
@@ -12,9 +12,34 @@ import Swal from 'sweetalert2';
 })
 export class FavoritosComponent implements OnInit {
   infoFavoritos:any[]=[];
-
- 
   datos!: infoUsuario;
+  esFavorito: boolean = false;
+
+  toggleFavorito() {
+    
+    if(this.isLoggedIn){
+      console.log("Id_usuario: "+ localStorage.getItem("Id_Usuario")+ "Id_Inmueble" + this.id_inmueble);
+      this.httpService.Favoritos(localStorage.getItem("Id_Usuario"), this.id_inmueble,"1").subscribe((data : any) =>{
+        if(data == 1){
+          this.esFavorito = !this.esFavorito;
+        Swal.fire({
+      
+          icon: 'success',
+          title: 'Se ha agregado a tus favoritos',
+         
+        })}else{
+          Swal.fire('Este inmueble, ya se encuentra en tus favoritos');
+        }
+      })
+      
+      } else{
+        Swal.fire('Inicia sesion para poder guardar favoritos');
+      } 
+  }
+
+  isLoggedIn: boolean= true;
+id_inmueble!: string;
+id_usuario!: String;
 
   constructor(public dialog: MatDialog,
     private httpService: HttpService,
@@ -24,6 +49,7 @@ export class FavoritosComponent implements OnInit {
   ngOnInit(): void {
 
     this.obtenerInfo();
+    
   
   }
   openDialog(): void {
@@ -33,8 +59,42 @@ export class FavoritosComponent implements OnInit {
   obtenerInfo(){
     this.httpService.obtenerInfoFavoritos(localStorage.getItem("Id_Usuario")).subscribe((data : any) =>{
   this.infoFavoritos=data;
+  console.log(this.infoFavoritos);
     })
   }
+
+  
+  Favoritos(Id_Inmueble:string)
+  {
+    if(this.isLoggedIn){
+    console.log("Id _ usuario"+ localStorage.getItem("Id_Usuario")+ "Id_Inmueble" + Id_Inmueble);
+    this.httpService.Favoritos(localStorage.getItem("Id_Usuario"), Id_Inmueble,"1").subscribe((data : any) =>{
+      if(data == 1){
+      
+      Swal.fire({
+    
+        icon: 'success',
+        title: 'Se ha agregado a tus favoritos',
+       
+      })
+      this.esFavorito = !this.esFavorito;}else{
+        this.httpService.borrarlikes(localStorage.getItem("Id_Usuario"),Id_Inmueble).subscribe((data: any)=>{
+          
+
+          this.esFavorito = !this.esFavorito;
+          Swal.fire('Este inmueble se quito de sus favoritos');
+          
+        })
+        this.obtenerInfo();
+      }
+    })
+    
+    } else{
+      Swal.fire('Inicia sesion para poder guardar favoritos');
+    } 
+
+  }
+  
  
  
 }
