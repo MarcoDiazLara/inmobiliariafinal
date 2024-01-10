bd8fdda39151a5412bbadbfdfe03b99ddbde0b59
betosau <betosau2112@gmail.com>
2024-01-10 12:21:44 -0600

    xD

--- src/app/inmueble/favoritos/favoritos.component.ts
+++ src/app/inmueble/favoritos/favoritos.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog} from '@angular/material/dialog';
+import { HttpService } from 'src/app/services/http/http.service';
+import { infoUsuario } from 'src/app/services/Interface/Interfaces';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-favoritos',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favoritos.component.scss']
 })
 export class FavoritosComponent implements OnInit {
+  infoFavoritos:any[]=[];
 
-  constructor() { }
+ 
+  datos!: infoUsuario;
+
+  constructor(public dialog: MatDialog,
+    private httpService: HttpService,
+    
+  ) { }
 
   ngOnInit(): void {
+
+    this.obtenerInfo();
+  
+  }
+  openDialog(): void {
+
   }
 
+  obtenerInfo(){
+    this.httpService.obtenerInfoFavoritos(localStorage.getItem("Id_Usuario")).subscribe((data : any) =>{
+  this.infoFavoritos=data;
+    })
+  }
+ 
+ 
 }
