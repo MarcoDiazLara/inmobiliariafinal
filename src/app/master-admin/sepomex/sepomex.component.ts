90d4f8c567cde4e0c02ddaa1bf2b35b42b764e1e
IIITunersIII <sanchezmota09@gmail.com>
2024-01-11 12:57:39 -0600

    hgytydrgfvhbjnklhuygngfdfgtrf

--- src/app/master-admin/sepomex/sepomex.component.ts
+++ src/app/master-admin/sepomex/sepomex.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-sepomex',
   templateUrl: './sepomex.component.html',
   styleUrls: ['./sepomex.component.scss']
 })
 export class SepomexComponent implements OnInit {
- 
+  panelOpenState = false;
+  
   constructor() { }
 
   ngOnInit(): void {
