c77a48978919b8157f549485aa27a78d897aa57b
Alejandra2050 <75100495+Alejandra2050@users.noreply.github.com>
2024-01-19 17:38:31 -0600

    xd

--- src/app/web/inmobiliariacorredor/inmobiliariacorredor.component.ts
+++ src/app/web/inmobiliariacorredor/inmobiliariacorredor.component.ts
@@ -5,6 +5,8 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MetododepagoComponent } from 'src/app/metododepago/metododepago.component';
 import { MatDialog } from '@angular/material/dialog';
+import { HttpService } from 'src/app/services/http/http.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inmobiliariacorredor',
@@ -12,19 +14,25 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./inmobiliariacorredor.component.css']
 })
 export class InmobiliariacorredorComponent implements OnInit {
+  isLoggedIn: boolean = false;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog , private httpService: HttpService, private router: Router) { }
 
   ngOnInit(): void {
-  
+    this.isLoggedIn = this.httpService.getGlobalVariable();
   }
   openDialog(bandera:number): void {
+    if(this.isLoggedIn){
+
+    
       let auxi=bandera.toString();
       localStorage.setItem("tipodeplan",auxi);
 
     const dialogRef = this.dialog.open(MetododepagoComponent, {
     });
-
+  }else{
+    this.router.navigate(['web/contacto']);
+  }
    
 }
 }
