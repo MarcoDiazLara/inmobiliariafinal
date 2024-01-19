583d23eafad1ef103069d39fc43cdd58e84500ef
AlmaJimenezGonz <144948872+AlmaJimenezGonz@users.noreply.github.com>
2024-01-19 13:04:41 -0600

    Modificaciones para responsive en catalogo inmueble

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -361,7 +361,6 @@ export class HttpService {
     return this.httpclient.post(this.url + 'consultar_detalle_inmuebles_asesores.php', params, { headers });
   }
 
-
   // mostrarDetallesInmueble(p_id_publicacion:any){
   //   let headers: any = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
   //   let params = 'Id_Publicacion='+ p_id_publicacion;
@@ -379,7 +378,6 @@ export class HttpService {
     return this.httpclient.post(this.url + 'obtenerNotis.php', params, { headers });
 
   }
-
 
   altaSocioCompleto(p_nombres: any, p_a_paterno: any, p_a_materno: any, p_nom_usuario: any, p_contrasena: any,
     p_correo: any, p_tel_fijo: any, p_cel: any, p_tipo_usuario: any, p_estatus: any, p_id_admin: any, p_desc_usu: any, p_RFC: any,
